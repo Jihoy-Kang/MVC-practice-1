c654a5691cd294a7233fdb7f9787e9489c068c37
Jihoy-Kang <luxuryjihyo@naver.com>
2022-09-03 17:18:29 +0900

    03 Sep 2022

---
+++ src/main/java/com/example/itemservice/web/basic/BasicItemController.java
@@ -0,0 +1,35 @@
+package com.example.itemservice.web.basic;
+
+import com.example.itemservice.domain.item.Item;
+import com.example.itemservice.domain.item.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
+
+@Controller
+@RequestMapping("/basic/items")
+@RequiredArgsConstructor // final 붙은 빈 생성자 만들어줌
+public class BasicItemController {
+
+    private final ItemRepository itemRepository; // @RequiredArgsConstructor로 생성자 자동 생성
+
+    @GetMapping
+    public String items(Model model){
+        List<Item> items = itemRepository.findAll();
+        model.addAttribute("items", items);
+        return "basic/items";
+    }
+
+    //테스트를 위한 데이터 추가
+    @PostConstruct
+    public void init(){
+        itemRepository.save(new Item("itemA", 10000, 10));
+        itemRepository.save(new Item("itemB", 20000, 20));
+    }
+
+}
